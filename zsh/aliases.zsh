53554c15ccb4c6a311867bc2d2e11117e79d93b6
Serhii Hopkalo <Sergey.Gopkalo@gmail.com>
2018-07-20 22:37:05 +0300

    alias for ls

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -41,6 +41,7 @@ elif [[ $platform == 'darwin' ]]; then
   alias ll='ls -alGh'
   alias ls='ls -Gh'
 fi
+alias l="ll"
 
 # show me files matching "ls grep"
 alias lsg='ll | grep'
@@ -132,7 +133,6 @@ alias gdmb='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 # Common shell functions
 alias less='less -r'
 alias tf='tail -f'
-alias l='less'
 alias lh='ls -alt | head' # see the last modified files
 alias screen='TERM=screen screen'
 alias cl='clear'
