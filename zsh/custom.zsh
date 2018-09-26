b6c11e4a9d0c71e078575c0aafce1d462a32b847
Serhii Hopkalo <Sergey Gopkalo@gmail.com>
2018-09-26 14:11:15 +0300

    Scripting for rails' convenience

--- zsh/custom.zsh
+++ zsh/custom.zsh
@@ -16,3 +16,26 @@ fi
 
 export HOMEBREW_GITHUB_API_TOKEN="63cdb22824c4a6f511f3eb9271c26f2c6fcd522b"
 
+function rails() {
+  if [ "$1" = "start" ]; then
+     if [ "$2" = "" ]; then
+        RENV="development"
+     else
+        RENV="$2"
+     fi
+     rails server -d -e "$RENV"
+     return 0
+  elif [ "$1" = "stop" ]; then
+     if [ -f tmp/pids/server.pid ]; then
+        kill $2 $(cat tmp/pids/server.pid)
+        return 0
+     else
+        echo "It seems there is no server running or you are not in a rails project root directory"
+        return 1
+     fi
+  elif [ "$1" = "restart" ]; then
+     rails stop && rails start $2
+  else
+     command rails $@
+  fi;
+}
